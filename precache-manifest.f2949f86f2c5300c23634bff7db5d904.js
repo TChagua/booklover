1de8d8c9b3c95c9c4212048f1b831806a976a8bc
TChagua <tea.chagua@gmail.com>
2019-02-16 20:52:43 +0400

    Updates

---
+++ precache-manifest.f2949f86f2c5300c23634bff7db5d904.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92b0a7cfaaffb894ecb5",
+    "url": "/booklover/static/js/runtime~main.92b0a7cf.js"
+  },
+  {
+    "revision": "f4820ab1e8115009d59b",
+    "url": "/booklover/static/js/main.f4820ab1.chunk.js"
+  },
+  {
+    "revision": "3943001d5c4a68d4c49e",
+    "url": "/booklover/static/js/2.3943001d.chunk.js"
+  },
+  {
+    "revision": "f4820ab1e8115009d59b",
+    "url": "/booklover/static/css/main.5af1ec6b.chunk.css"
+  },
+  {
+    "revision": "bbc43716db4278df3d851687f08de54d",
+    "url": "/booklover/index.html"
+  }
+];
